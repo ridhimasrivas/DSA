683649344b28e78435377be6200683474fb36847
Ridhima Srivastava <ridhimasri20@gmail.com>
2026-03-29 12:14:52 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.23%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int calc(int i,int j,vector<vector<int>>&dp,vector<vector<int>>& obstacleGrid)
+    {
+        if(i<0||j<0||obstacleGrid[i][j]==1)
+        return 0;
+        if(i==0 && j==0)
+        return 1;
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int up=calc(i-1,j,dp,obstacleGrid);
+        int left =calc(i,j-1,dp,obstacleGrid);
+        return dp[i][j]=up+left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();        
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return calc(m-1,n-1,dp,obstacleGrid);
+    }
+};
