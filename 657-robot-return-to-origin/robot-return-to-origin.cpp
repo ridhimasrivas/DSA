c31d83aafcb63379553d4e126ee316e6694435d7
Ridhima Srivastava <ridhimasri20@gmail.com>
2026-04-07 22:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (75.86%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int u = 0,  d = 0, l = 0, r = 0;
+        for (int i = 0; i < moves.size(); i++) {
+            if (moves[i] == 'U')
+                u++;
+            else if (moves[i] == 'D')
+                d++;
+            else if (moves[i] == 'L')
+                l++;
+            else
+                r++;
+            
+        }
+        if (u == d && r == l)
+            return true;
+        else
+            return false;
+    }
+};
