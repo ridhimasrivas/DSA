75661b4dab6c60c6a417166509fa3cece92c1ecb
Ridhima Srivastava <ridhimasri20@gmail.com>
2026-08-03 01:42:20 +0530

    Time: 15 ms (21.37%) | Memory: 19.7 MB (11.02%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,27 @@
 class Solution {
+private:
+    vector<vector<int>> memo;
+
+    int maxDifference(vector<int>& piles, int left, int right) {
+        if(left > right) {
+            return 0;
+        }
+
+        if (memo[left][right] != -1) {
+            return memo[left][right];
+        }
+
+        int pickLeft = piles[left] - maxDifference(piles, left + 1, right);
+        int pickRight = piles[right] - maxDifference(piles, left, right - 1);
+
+        return memo[left][right] = max(pickLeft, pickRight);
+    }
+
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        memo.assign(n, vector<int>(n, -1));
+        
+        return maxDifference(piles, 0, n - 1) > 0;
     }
 };
