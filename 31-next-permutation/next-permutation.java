78de6ce9552885b0277d2cb1ef5f3a345a322530
Ridhima Srivastava <ridhimasri20@gmail.com>
2026-08-07 16:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (52.63%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,45 +1,30 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind1=-1;
-        int ind2=-1;
-        // step 1 find breaking point 
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind1=i;
-                break;
-            }
+        int n = nums.length;
+        int i= n-2;
+        while (i>=0 && nums[i] >= nums[i+1]){
+            i-=1;
         }
-        // if there is no breaking  point 
-        if(ind1==-1){
-            reverse(nums,0);
-        }
-        
-        else{
-            // step 2 find next greater element and swap with ind2
-            for(int i=nums.length-1;i>=0;i--){
-                if(nums[i]>nums[ind1]){
-                    ind2=i;
-                    break;
-                }
-            }
-
-            swap(nums,ind1,ind2);
-            // step 3 reverse the rest right half
-            reverse(nums,ind1+1);
-        }
-    }
-    void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
-    void reverse(int[] nums,int start){
-        int i=start;
-        int j=nums.length-1;
-        while(i<j){
-            swap(nums,i,j);
-            i++;
+        if (i>=0){
+        int j = n-1;
+        while ( nums[j] <= nums[i]){
             j--;
         }
+            //swap
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        //reverse the elements after i
+        int left = i + 1;
+        int right = n - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+        
     }
 }
